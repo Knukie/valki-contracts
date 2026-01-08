263b3bd4f6b2fcbf5a47ece436e6589ca7bd40da
Knukie <ianvdvalk@gmail.com>
2026-01-08 12:29:00 +0100

    Add ImageMetadata type alias export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
 export type { ImageMeta } from "./image.js";
+export type { ImageMeta as ImageMetadata } from "./image.js";
 export type { Message, Role } from "./message.js";
 export type { Conversation, ConversationStatus } from "./conversation.js";
 export type { User, UserRole, UserStatus } from "./user.js";
